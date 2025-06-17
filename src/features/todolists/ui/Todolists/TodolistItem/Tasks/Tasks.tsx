5de9aaefff5663940d24c9f326b0cd72f0e4358e
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-17 14:03:19 +0300

    change tasks

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -1,38 +1,48 @@
-import { TaskStatus } from "@/common/enums"
-import { useGetTasksQuery } from "@/features/todolists/api/tasksApi"
-import type { DomainTodolist } from "@/features/todolists/lib/types"
+import {TaskStatus} from "@/common/enums"
+import {useGetTasksQuery} from "@/features/todolists/api/tasksApi"
+import type {DomainTodolist} from "@/features/todolists/lib/types"
 import List from "@mui/material/List"
-import { TaskItem } from "./TaskItem/TaskItem"
-import { TasksSkeleton } from "./TasksSkeleton/TasksSkeleton"
+import {TaskItem} from "./TaskItem/TaskItem"
+import {TasksSkeleton} from "./TasksSkeleton/TasksSkeleton"
+import {useState} from "react";
+import {TasksPagination} from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPaginaton/TasksPaginaton.tsx";
 
 type Props = {
-  todolist: DomainTodolist
+    todolist: DomainTodolist
 }
 
-export const Tasks = ({ todolist }: Props) => {
-  const { id, filter } = todolist
+export const Tasks = ({todolist}: Props) => {
+    const {id, filter} = todolist
 
-  const { data, isLoading } = useGetTasksQuery(id)
+    const [page, setPage] = useState<number>(1)
 
-  let filteredTasks = data?.items
-  if (filter === "active") {
-    filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.New)
-  }
-  if (filter === "completed") {
-    filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.Completed)
-  }
+    // const { data, isLoading } = useGetTasksQuery({todolistId: id, params: {count:3, page:2}})
+    const {data, isLoading} = useGetTasksQuery({todolistId: id, params: {page}})
 
-  if (isLoading) {
-    return <TasksSkeleton />
-  }
+    let filteredTasks = data?.items
+    if (filter === "active") {
+        filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.New)
+    }
+    if (filter === "completed") {
+        filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.Completed)
+    }
 
-  return (
-    <>
-      {filteredTasks?.length === 0 ? (
-        <p>Тасок нет</p>
-      ) : (
-        <List>{filteredTasks?.map((task) => <TaskItem key={task.id} task={task} todolist={todolist} />)}</List>
-      )}
-    </>
-  )
+    if (isLoading) {
+        return <TasksSkeleton/>
+    }
+
+    return (
+        <>
+            {filteredTasks?.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <>
+                    <List>
+                        {filteredTasks?.map((task) => <TaskItem key={task.id} task={task} todolist={todolist}/>)}
+                    </List>
+                    <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage} />
+                </>
+            )}
+        </>
+    )
 }
