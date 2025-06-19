0c9c6c13877be2082a20efb074217ceef8f8c0db
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-19 13:49:23 +0300

    change loginSchema

--- src/features/auth/lib/schemas/loginSchema.ts
+++ src/features/auth/lib/schemas/loginSchema.ts
@@ -1,12 +1,30 @@
 import { z } from "zod"
 
-export const loginSchema = z.object({
+// export const loginSchema = z.object({
+//   email: z.string().min(1, { message: "Email is required" }).email({ message: "Incorrect email address" }),
+//   password: z
+//     .string()
+//     .min(1, { message: "Password is required" })
+//     .min(3, { message: "Password must be at least 3 characters long" }),
+//   rememberMe: z.boolean(),
+//   captcha: z.string().optional()
+// })
+
+const baseSchema = z.object({
   email: z.string().min(1, { message: "Email is required" }).email({ message: "Incorrect email address" }),
   password: z
-    .string()
-    .min(1, { message: "Password is required" })
-    .min(3, { message: "Password must be at least 3 characters long" }),
+      .string()
+      .min(1, { message: "Password is required" })
+      .min(3, { message: "Password must be at least 3 characters long" }),
   rememberMe: z.boolean(),
+})
+
+export const loginSchema = baseSchema.extend({
+  captcha: z.string().optional()
+})
+
+export const loginWithCaptchaSchema = baseSchema.extend({
+  captcha: z.string().min(1, { message: "Captcha is required" })
 })
 
 export type Inputs = z.infer<typeof loginSchema>
