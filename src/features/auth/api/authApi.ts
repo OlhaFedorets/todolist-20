1f4bc1adfa657b2153ca7fd932d2cfc35ee4acb6
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-19 13:48:46 +0300

    change authApi

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,13 +1,14 @@
 import { baseApi } from "@/app/baseApi"
 import type { BaseResponse } from "@/common/types"
-import type { LoginArgs } from "./authApi.types"
+// import type { LoginArgs } from "./authApi.types"
+import {Inputs} from "@/features/auth/lib/schemas";
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     me: build.query<BaseResponse<{ id: number; email: string; login: string }>, void>({
       query: () => "auth/me",
     }),
-    login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginArgs>({
+    login: build.mutation<BaseResponse<{ userId: number; token: string }>, Inputs>({
       query: (body) => ({
         url: "auth/login",
         method: "POST",
@@ -20,7 +21,10 @@ export const authApi = baseApi.injectEndpoints({
         method: "DELETE",
       }),
     }),
+    captcha: build.query<{url: string}, void>({
+      query: () => "/security/get-captcha-url",
+    })
   }),
 })
 
-export const { useMeQuery, useLoginMutation, useLogoutMutation } = authApi
+export const { useMeQuery, useLoginMutation, useLogoutMutation, useLazyCaptchaQuery } = authApi
